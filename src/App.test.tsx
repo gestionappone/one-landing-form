9c0877ab09e96756fa627e5ec4a48ae744ffaaa3
Emanuel <emanuelvm04@gmail.com>
2024-08-19 18:21:07 +0200

    Configuring APP before start dev

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders welcome message", () => {
+  render(<App />);
+  expect(screen.getByText("Vite + React")).toBeInTheDocument();
+});
