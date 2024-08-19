785c2ef93964f2b72f291e221e06aade25f93cc8
Emanuel <emanuelvm04@gmail.com>
2024-08-19 19:14:45 +0200

    Keep calm and commit

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders welcome message", () => {
-  render(<App />);
-  expect(screen.getByText("Vite + React")).toBeInTheDocument();
-});
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
@@ -6,6 +7,11 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), viteReact()],
+  test: {
+    globals: true, // Habilita los métodos globales como describe, test, etc.
+    environment: "jsdom", // Usa jsdom como entorno de pruebas
+    setupFiles: "./src/setupTests.ts", // Archivo para configuraciones globales
+  },
   resolve: {
     alias: {
       "*": path.resolve(__dirname, "./src"),
